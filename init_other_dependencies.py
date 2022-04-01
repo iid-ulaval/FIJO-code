6b5a95cb4c2aeafbf8ab25b443f92cc8e8d81daf
davebulaval <david.beauchemin.5@ulaval.ca>
2022-04-01 12:08:05 -0400

    add removal of zip file

--- init_other_dependencies.py
+++ init_other_dependencies.py
@@ -1,8 +1,9 @@
+import os
 import shutil
 
+import fasttext.util
 import nltk
 import spacy
-import fasttext.util
 
 print("Starting of init of other dependencies.")
 
@@ -13,5 +14,6 @@ spacy.cli.download("fr_core_news_md")
 
 fasttext.util.download_model('fr', if_exists='ignore')
 shutil.move("cc.fr.300.bin", "./experiment/embedding")
+os.remove("cc.fr.300.bin.gz")
 
 print("Init of other dependencies done.")
