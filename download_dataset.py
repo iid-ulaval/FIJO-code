28d29e60cdab4c3706dd6930332a7b04bd4eeb62
davebulaval <david.beauchemin.5@ulaval.ca>
2022-04-01 12:32:30 -0400

    fix out file of zip

--- download_dataset.py
+++ download_dataset.py
@@ -10,5 +10,5 @@ wget.download(url, out=download_out_file_path)
 
 extract_out_file_path = "../data/fijo.json"
 with zipfile.ZipFile(download_out_file_path) as zip_file:
-    zip_file.extractall(zip_file)
+    zip_file.extractall(extract_out_file_path)
     print("Extracted all")
